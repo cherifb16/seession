9d23e5dd6de15cd2b1211591b2d0e2ad959bc8b8
cherifb16 <cherifbobbyray@gmail.com>
2023-05-02 11:55:16 +0200

    adding association users-tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 6 }
 
-  has_secure_password  
+  has_secure_password 
+  
+  has_many :tasks
 end
